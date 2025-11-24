921b84c2f5ff8beabb5779980c22b797b8285151
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2025-11-24 16:49:40 -0500

    Null check

--- webgpu.c
+++ webgpu.c
@@ -577,6 +577,7 @@ void wgpuCommandEncoderRelease(WGPUCommandEncoder commandEncoder)
 void wgpuComputePassEncoderDispatchWorkgroups(WGPUComputePassEncoder computePassEncoder, uint32_t workgroupCountX,
     uint32_t workgroupCountY, uint32_t workgroupCountZ)
 {
+    if(!computePassEncoder) unreachable();
     wasi_webgpu_webgpu_method_gpu_compute_pass_encoder_dispatch_workgroups(
         wasi_webgpu_webgpu_borrow_gpu_compute_pass_encoder(computePassEncoder->compute_pass_encoder),
         workgroupCountX,
